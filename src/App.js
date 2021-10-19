e1b67d5d10b4a2c5c85e1c9e24df9bf9c121fdc9
luis artur <luisdallacquaa@gmail.com>
2021-10-19 11:52:36 -0300

    Add BookList, hook and BookDetail components with tests

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import React from 'react'
 import { Typography } from '@material-ui/core'
 
-function App() {
+import { Route, Switch } from 'react-router-dom'
+import BookListContainer from './BookListContainer'
+import BookDetailContainer from './BookDetailContainer'
+
+const App = () => {
+ 
   return (
     <div>
       <Typography variant='h2' component='h2' data-test='heading'>
         Bookish
       </Typography>
+      <Switch>
+        <Route exact path='/' component={BookListContainer} />
+        <Route path='/books/:id' component={BookDetailContainer} />
+      </Switch>
     </div>
   );
 }
---
+++ src/BookListContainer.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import BookList from './BookList'
+import { useRemoteService } from './hooks';
+
+const BookListContainer = () => {
+    const { data, loading, error } = useRemoteService([]);   
+    return <BookList books={data} loading={loading} error={error} />
+}
+
+export default BookListContainer
